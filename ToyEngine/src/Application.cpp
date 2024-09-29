f44e4cf7008acd57d7a5b95b9478e8485d2cc670
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2024-09-29 16:47:46 -0400

    Create window and render triangle.

--- ToyEngine/src/Application.cpp
+++ ToyEngine/src/Application.cpp
@@ -2,11 +2,25 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0 ) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0"; 
+
 void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main(void) {
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello Triangle" << std::endl;
 
 	// Initialize GLFW
 	glfwInit(); 
@@ -15,7 +29,7 @@ int main(void) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	// 
+	// Specify window dimensions
 	int windowWidth = 800; 
 	int windowHeight = 600; 
 
@@ -39,8 +53,86 @@ int main(void) {
 	// Set rendering window size 
 	glViewport(0, 0, windowWidth, windowHeight); 
 
-	// 
+	// set call-back function for window resize
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// generate vertex array object (VAO) to store vertex attributes
+	unsigned int VAO; 
+	glGenVertexArrays(1, &VAO); 
+	glBindVertexArray(VAO);
+
+	// specify vertex data  
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f,	// top right
+		 0.5f, -0.5f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f,	// bottom left
+		-0.5f,  0.5f, 0.0f,	// top left
+	}; 
+
+	// create Vertex Buffer Object (VBO)
+	unsigned int VBO; 
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); 
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); 
+	glEnableVertexAttribArray(0); 
+
+	//  Element Buffer Object (EBO)
+	unsigned int indices[] = {
+		0,1,3,	// first triangle
+		1,2,3,	// second triangle
+	};
+
+	unsigned int EBO; 
+	glGenBuffers(1, &EBO); 
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); 
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+
+	// compile shaders
+	unsigned int vertexShader; 
+	vertexShader = glCreateShader(GL_VERTEX_SHADER); 
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); 
+	glCompileShader(vertexShader);
+
+	int success; 
+	char infoLog[512]; 
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); 
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog); 
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\N" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader; 
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); 
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); 
+	glCompileShader(fragmentShader); 
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success); 
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog); 
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int shaderProgram; 
+	shaderProgram = glCreateProgram(); 
+
+	glAttachShader(shaderProgram, vertexShader); 
+	glAttachShader(shaderProgram, fragmentShader); 
+	glLinkProgram(shaderProgram); 
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success); 
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog); 
+		std::cout << "ERROR::SHADERPROGRAM::COMPILIATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	// Render loop 
 	while (!glfwWindowShouldClose(window))
@@ -52,10 +144,15 @@ int main(void) {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT); 
 
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); 
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 		// check and call events and swap buffers
 		glfwPollEvents();
 		glfwSwapBuffers(window); 
-		
 	}
 
 	// Terminate window
