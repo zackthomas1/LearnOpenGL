aea087a7b3116393c35b9486f967695df84f3b7b
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2024-11-28 13:42:46 -0500

    Create precompiled header file.

---
+++ ToyEngine/src/pch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <fstream> 
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+
+#ifdef PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
+
