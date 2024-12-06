500f2bfa44b04eb53410cf24ae3ce4a5304a3878
zackthomas1 <31631098+zackthomas1@users.noreply.github.com>
2024-12-06 15:27:40 -0500

    Create delta time class to encapsulate time step data about frame refresh rates.

---
+++ ToyEngine/src/delta_time.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "delta_time.h"
+#include <GLFW/glfw3.h>
+
+namespace ToyEngine {
+	DeltaTime::DeltaTime()
+	{
+		current_frame_time_ = glfwGetTime();
+		delta_time_ = 0.0f; 
+		last_frame_time_ = current_frame_time_;
+	}
+
+	void DeltaTime::Step()
+	{
+		current_frame_time_ = glfwGetTime();
+		delta_time_ = current_frame_time_ - last_frame_time_;
+		last_frame_time_ = current_frame_time_;
+		//std::cout << delta_time_ << std::endl;
+	}
+}
