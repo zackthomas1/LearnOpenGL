8be0aa0e8ab1bc28dc57c9695347e5aa23b0eb2c
zackthomas1 <zackthomas13@gmail.com>
2025-06-25 12:37:40 -0700

    Complete premake build script for assimp submodule.

--- LearnOpenGL/vendor/premake/assimp.lua
+++ LearnOpenGL/vendor/premake/assimp.lua
@@ -11,77 +11,66 @@ project "assimp"
     targetdir (bindir)
     objdir (binintdir)
 
+    local function addCMakeBuild(cfgFilter)
+        local cmakeFlags = {
+            "-DASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT=ON",
+            "-DASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT=ON",
+            "-DASSIMP_BUILD_ZLIB=ON",
+            "-DASSIMP_INSTALL=OFF",
+            "-DBUILD_SHARED_LIBS=OFF",
+            "-DUSE_STATIC_CRT=ON",
+            "-DCMAKE_DEBUG_POSTFIX=",
+            "-DLIBRARY_SUFFIX=",
+            "-DCMAKE_BUILD_TYPE=" .. cfgFilter,
+        }
+        local flagsString = table.concat(cmakeFlags, " ")
+
+        -- 1) Configure step: Generate a VS solution
+        buildmessage(" Generating VS solution and building binary (static lib)...")
+        buildcommands {
+            "cmake -S " .. assimpdir ..
+            " -B " .. assimpbuilddir ..
+            " -G \"Visual Studio 16 2019\" -A x64" ..
+            " " .. flagsString
+        }
+
+        -- 2) Build step
+        buildcommands {
+            "cmake --build " .. assimpbuilddir .. " --config " .. cfgFilter
+        }
+
+        -- 3) Tell Premake what file to expect as output
+        buildoutputs { 
+            path.join(assimpbuilddir, "assimp.lib"),
+            path.join(assimpbuilddir, (cfgFilter=="Debug" and "zlibstaticd.lib" or "zlibstatic.lib")),
+        }
+
+        -- 4) copy libs to bin/ directory
+        -- copy Assimp static lib
+        local assimplib = path.join(assimpbuilddir, "lib", cfgFilter, "assimp.lib")
+        postbuildmessage (('{COPY} "%s" "%s"'):format(assimplib, bindir))
+        postbuildcommands {
+            ('{COPY} "%s" "%s"'):format(assimplib, bindir),
+        }
+        
+        -- copy ZLib static lib
+        local zliblib   = path.join(assimpbuilddir, "contrib", "zlib", cfgFilter)
+        local zlibname  = (cfgFilter=="Debug" and "zlibstaticd.lib" or "zlibstatic.lib")
+        postbuildmessage (('{COPY} "%s"->"%s"'):format(zliblib, bindir))
+        postbuildcommands{
+            ('{COPY} "%s" "%s"'):format(path.join(zliblib, zlibname), bindir),
+        }
+    end
+
     filter "configurations:Debug"
         runtime "Debug"
         symbols "on"
+        addCMakeBuild("Debug")
     filter "configurations:Release"
         runtime "Release"
         optimize "speed"
+        addCMakeBuild("Release")
     filter "configurations:Dist"
         runtime "Release"
         optimize "On"
-    filter{}
-    
-    local cmakeFlags = {
-        "-DASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT=ON",
-        "-DASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT=ON",
-        "-DASSIMP_BUILD_ZLIB=ON",
-        "-DASSIMP_INSTALL=OFF",
-        "-DBUILD_SHARED_LIBS=OFF",
-        "-DUSE_STATIC_CRT=ON",
-        "-DCMAKE_DEBUG_POSTFIX=",
-        "-DLIBRARY_SUFFIX=",
-        "-DCMAKE_BUILD_TYPE=%{cfg.buildcfg}",
-    }
-    local flagsString = table.concat(cmakeFlags, " ")
-
-    -- 1) Configure step: Generate a VS solution
-    buildmessage(" Generating VS solution and building binary (static lib)...")
-    local assimplib = path.join(assimpbuilddir, "lib", "%{cfg.buildcfg}", "assimp.lib")
-    local zliblib   = path.join(assimpbuilddir, "contrib", "zlib", "%{cfg.buildcfg}", "zlibstaticd.lib")
-
-    buildcommands {
-        "cmake -S " .. assimpdir ..
-        " -B " .. assimpbuilddir ..
-        " -G \"Visual Studio 16 2019\" -A x64" ..
-        " " .. flagsString
-    }
-
-    -- 2) Build step
-    buildcommands {
-        "cmake --build " .. assimpbuilddir .. " --config %{cfg.buildcfg}"
-    }
-
-    -- 3) Tell Premake what file to expect as output
-    buildoutputs { "%{cfg.buildcfg}/assimp.lib" }
-
-    -- 4) copy libs to bin/ directory
-    -- copy the Assimp static lib
-    local assimplib = path.join(assimpbuilddir, "lib", "%{cfg.buildcfg}", "assimp.lib")
-    postbuildcommands {
-        ('{COPY} "%s" "%s"'):format(assimplib, bindir),
-    }
-    postbuildmessage (('{COPY} "%s" "%s"'):format(assimplib, bindir))
-    
-    -- copy the ZLib static lib
-    local zliblib   = path.join(assimpbuilddir, "contrib", "zlib", "%{cfg.buildcfg}")
-    filter "configurations:Debug"
-        postbuildcommands{
-            ('{COPY} "%s" "%s"'):format(path.join(zliblib, "zlibstaticd.lib"), bindir),
-        }
-    filter "configurations:Release"
-        postbuildcommands{
-            ('{COPY} "%s" "%s"'):format(path.join(zliblib, "zlibstatic.lib"), bindir),
-        }
-    filter "configurations:Dist"
-        postbuildcommands{
-            ('{COPY} "%s" "%s"'):format(path.join(zliblib, "zlibstatic.lib"), bindir),
-        }
-    filter{}
-    postbuildmessage (('{COPY} "%s" "%s"'):format(zliblib, bindir))
-
-    -- 5) Remove temp build directories
-    postbuildcommands {
-        ('{RMDIR} "%s"'):format(path.join( vendordir, "premake", "%{cfg.buildcfg}" ))
-    }
-    postbuildmessage (('{RMDIR} "%s"'):format(path.join( vendordir, "premake", "%{cfg.buildcfg}" )))
+        addCMakeBuild("Release")
