e70664e4759f278c513c937bfb12f486bd3c4f19
zackthomas1 <zackthomas13@gmail.com>
2026-07-04 18:08:23 -0700

    Renamed default shader to blinn_phong shader. Replaced position point light with directional light. Normalized lightdir before calculating the reflection direction.

--- assets/shaders/5_3_blinn_phong.fs
+++ assets/shaders/5_3_blinn_phong.fs
@@ -9,7 +9,7 @@ in VS_OUT
 } fs_in;
 
 uniform sampler2D floorTexture; 
-uniform vec3 lightPos;
+uniform vec3 lightDir;
 uniform vec3 lightColor;
 uniform vec3 viewPos; 
 uniform bool isBlinn;
@@ -23,14 +23,13 @@ void main()
     vec3 ambient = 0.05 * color; 
 
     // diffuse
-    vec3 lightDir = normalize(lightPos - fs_in.FragPos); 
     vec3 normal = normalize(fs_in.Normal); 
     float cosineTerm = max(dot(lightDir, normal), 0.0);
     vec3 diffuse = color * cosineTerm;
 
     //specular
     vec3 viewDir = normalize(viewPos - fs_in.FragPos); 
-    vec3 reflectDir = reflect(-lightDir, normal); 
+    vec3 reflectDir = reflect(normalize(-lightDir), normal); 
     float spec = 0.0; 
     if(isBlinn){
         vec3 halfwayDir = normalize(lightDir + viewDir); 
@@ -40,11 +39,11 @@ void main()
     }
     vec3 specular = vec3(0.3) * spec;
 
-    // attenuation
-    float dist = length(lightPos - fs_in.FragPos);
-    float attenuation = 1.0 / (isGamma ? dist * dist : dist);
-    diffuse *= attenuation;
-    specular *= attenuation;
+    // // attenuation
+    // float dist = length(lightPos - fs_in.FragPos);
+    // float attenuation = 1.0 / (isGamma ? dist * dist : dist);
+    // diffuse *= attenuation;
+    // specular *= attenuation;
 
     // final result
     vec3 result = diffuse + specular;
